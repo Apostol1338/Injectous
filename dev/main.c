075b1036ccacc6b88991e9d14f5bf5c59dc61ea6
Danylo Yurov <aveyurov@gmail.com>
2019-05-01 21:39:20 +0300

    Implements basic modify & write logic

--- dev/main.c
+++ dev/main.c
@@ -2,6 +2,19 @@
 #include <jpeglib.h>
 #include <setjmp.h>
 
+const char* data = "XYRNDOK!";
+const size_t kDataSize = 8;
+
+enum InteractionType {
+  kModify,
+  kRead
+};
+
+GLOBAL(void)
+write_DCT_coef(const char* filename, struct jpeg_decompress_struct* in_cinfo, jvirt_barray_ptr* coeffs_array);
+GLOBAL(void)
+open_DCT_coefs(const char* filename, enum InteractionType interaction_type);
+
 struct my_error_mgr {
   struct jpeg_error_mgr pub;	/* "public" fields */
 
@@ -23,156 +36,17 @@ my_error_exit (j_common_ptr cinfo) {
   longjmp(myerr->setjmp_buffer, 1);
 }
 
-GLOBAL(int)
-read_JPEG_file (char* filename) {
-  /* This struct contains the JPEG decompression parameters and pointers to
-   * working space (which is allocated as needed by the JPEG library).
-   */
-  struct jpeg_decompress_struct cinfo;
-  /* We use our private extension JPEG error handler.
-   * Note that this struct must live as long as the main JPEG parameter
-   * struct, to avoid dangling-pointer problems.
-   */
-  struct my_error_mgr jerr;
-  /* More stuff */
-  FILE * infile;		/* source file */
-  JSAMPARRAY buffer;		/* Output row buffer */
-  int row_stride;		/* physical row width in output buffer */
-
-  /* In this example we want to open the input file before doing anything else,
-   * so that the setjmp() error recovery below can assume the file is open.
-   * VERY IMPORTANT: use "b" option to fopen() if you are on a machine that
-   * requires it in order to read binary files.
-   */
-
-  if ((infile = fopen(filename, "rb")) == NULL) {
-    fprintf(stderr, "can't open %s\n", filename);
-    return 0;
-  }
-
-  /* Step 1: allocate and initialize JPEG decompression object */
-
-  /* We set up the normal JPEG error routines, then override error_exit. */
-  cinfo.err = jpeg_std_error(&jerr.pub);
-  jerr.pub.error_exit = my_error_exit;
-  /* Establish the setjmp return context for my_error_exit to use. */
-  if (setjmp(jerr.setjmp_buffer)) {
-    /* If we get here, the JPEG code has signaled an error.
-     * We need to clean up the JPEG object, close the input file, and return.
-     */
-    jpeg_destroy_decompress(&cinfo);
-    fclose(infile);
-    return 0;
-  }
-  /* Now we can initialize the JPEG decompression object. */
-  jpeg_create_decompress(&cinfo);
-
-  /* Step 2: specify data source (eg, a file) */
-
-  jpeg_stdio_src(&cinfo, infile);
-
-  /* Step 3: read file parameters with jpeg_read_header() */
-
-  (void) jpeg_read_header(&cinfo, TRUE);
-  /* We can ignore the return value from jpeg_read_header since
-   *   (a) suspension is not possible with the stdio data source, and
-   *   (b) we passed TRUE to reject a tables-only JPEG file as an error.
-   * See libjpeg.txt for more info.
-   */
-
-  /* Step 4: set parameters for decompression */
-
-  /* In this example, we don't need to change any of the defaults set by
-   * jpeg_read_header(), so we do nothing here.
-   */
-
-  /* Step 5: Start decompressor */
-
-  (void) jpeg_start_decompress(&cinfo);
-  /* We can ignore the return value since suspension is not possible
-   * with the stdio data source.
-   */
-
-  /* We may need to do some setup of our own at this point before reading
-   * the data.  After jpeg_start_decompress() we have the correct scaled
-   * output image dimensions available, as well as the output colormap
-   * if we asked for color quantization.
-   * In this example, we need to make an output work buffer of the right size.
-   */ 
-  /* JSAMPLEs per row in output buffer */
-  row_stride = cinfo.output_width * cinfo.output_components;
-  /* Make a one-row-high sample array that will go away when done with image */
-  buffer = (*cinfo.mem->alloc_sarray)
-		((j_common_ptr) &cinfo, JPOOL_IMAGE, row_stride, 1);
-
-  /* Step 6: while (scan lines remain to be read) */
-  /*           jpeg_read_scanlines(...); */
-
-  /* Here we use the library's state variable cinfo.output_scanline as the
-   * loop counter, so that we don't have to keep track ourselves.
-   */
-  // FILE* output;
-  // output = fopen("output.txt", "rw");
-
-  while (cinfo.output_scanline < cinfo.output_height) {
-    /* jpeg_read_scanlines expects an array of pointers to scanlines.
-     * Here the array is only one element long, but you could ask for
-     * more than one scanline at a time if that's more convenient.
-     */
-    (void) jpeg_read_scanlines(&cinfo, buffer, 1);
-    /* Assume put_scanline_someplace wants a pointer and sample count. */
-    //put_scanline_someplace(buffer[0], row_stride);
-    //printf("%s", buffer[0]);
-    printf("%s\n", buffer[0]);
-
-    int i = 0;
-    for(; i < row_stride; i++) {
-      printf("%d", (int)buffer[0][i]);
-    }
-    //fwrite(buffer[i++], row_stride, 7, output);
-  }
-
-  //fclose(output);
-
-  /* Step 7: Finish decompression */
-
-  (void) jpeg_finish_decompress(&cinfo);
-  /* We can ignore the return value since suspension is not possible
-   * with the stdio data source.
-   */
-
-  /* Step 8: Release JPEG decompression object */
-
-  /* This is an important step since it will release a good deal of memory. */
-  jpeg_destroy_decompress(&cinfo);
-
-  /* After finish_decompress, we can close the input file.
-   * Here we postpone it until after no more JPEG errors are possible,
-   * so as to simplify the setjmp error logic above.  (Actually, I don't
-   * think that jpeg_destroy can do an error exit, but why assume anything...)
-   */
-  fclose(infile);
-
-  /* At this point you may want to check to see whether any corrupt-data
-   * warnings occurred (test whether jerr.pub.num_warnings is nonzero).
-   */
-
-  /* And we're done! */
-  return 1;
-}
-
-GLOBAL(int)
-read_DCT_coef(const char* filename, const char* output_filename) {
+GLOBAL(void)
+open_DCT_coefs(const char* filename, enum InteractionType interaction_type) {
   struct jpeg_decompress_struct cinfo;
   struct my_error_mgr jerr;
   FILE* infile;
-  FILE* outfile;
   JSAMPARRAY buffer;
-  int row_stride;
+  jvirt_barray_ptr* coeffs_array;
 
   if ((infile = fopen(filename, "rb")) == NULL) {
     fprintf(stderr, "can't open %s\n", filename);
-    return 0;
+    return;
   }
 
   cinfo.err = jpeg_std_error(&jerr.pub);
@@ -181,54 +55,53 @@ read_DCT_coef(const char* filename, const char* output_filename) {
   if (setjmp(jerr.setjmp_buffer)) {
     jpeg_destroy_decompress(&cinfo);
     fclose(infile);
-    return 0;
+    return;
   }
 
   jpeg_create_decompress(&cinfo);
   jpeg_stdio_src(&cinfo, infile);
   jpeg_read_header(&cinfo, FALSE);
-  jvirt_barray_ptr* src_coef_arrays = jpeg_read_coefficients(&cinfo);
-
-  if ((outfile = fopen(output_filename, "w")) == NULL) {
-    fprintf(stderr, "can't open %s\n", output_filename);
-    return 0;
-  }
+  coeffs_array = jpeg_read_coefficients(&cinfo);
 
   JBLOCKARRAY rowPtrs[MAX_COMPONENTS];
 
-  for (JDIMENSION compNum=0; compNum < cinfo.num_components; compNum++) {
-    size_t blockRowSize = (size_t) sizeof(JCOEF) * DCTSIZE2 * cinfo.comp_info[compNum].width_in_blocks;
-    for (JDIMENSION rowNum=0; rowNum < cinfo.comp_info[compNum].height_in_blocks; rowNum++) {
-      // A pointer to the virtual array of dct values
-      rowPtrs[compNum] = ((&cinfo)->mem->access_virt_barray)((j_common_ptr) &cinfo, src_coef_arrays[compNum],rowNum, (JDIMENSION) 1, FALSE);
-      // Loop through the blocks to get the dct values
-      for (JDIMENSION blockNum=0; blockNum < cinfo.comp_info[compNum].width_in_blocks; blockNum++){
-        //...iterate over DCT coefficients
-        for (JDIMENSION i=0; i<DCTSIZE2; i++){
-          //and print them to standard out - one per line
-          fprintf(outfile, "%d", rowPtrs[compNum][0][blockNum][i]);
-        }
+  rowPtrs[0] = ((&cinfo)->mem->access_virt_barray)((j_common_ptr) &cinfo, coeffs_array[0], 0, (JDIMENSION) 1, FALSE);
 
-        fprintf(outfile, "\n");
-      }
-    }
+  for (int i = 0; i < kDataSize; i++) {
+
+    interaction_type == kModify ? rowPtrs[0][0][0][i] = data[i] : printf("%c", rowPtrs[0][0][0][i]);
   }
 
-  fclose(outfile);
+  if(kModify == interaction_type) {
+    write_DCT_coef(filename, &cinfo, coeffs_array);
+  }
+}
+
+GLOBAL(void)
+write_DCT_coef(const char* filename, struct jpeg_decompress_struct* in_cinfo, jvirt_barray_ptr* coeffs_array) {
+  struct jpeg_compress_struct cinfo;
+  struct jpeg_error_mgr jerr;
+  FILE* infile;
+
+  if((infile = fopen(filename, "wb")) == NULL) {
+    fprintf(stderr, "can't open %s\n", filename);
+    return;
+  }
+
+  cinfo.err = jpeg_std_error(&jerr);
+  jpeg_create_compress(&cinfo);
+  jpeg_stdio_dest(&cinfo, infile);
+  jpeg_copy_critical_parameters(in_cinfo, &cinfo);
+  jpeg_write_coefficients(&cinfo, coeffs_array);
+
+  jpeg_finish_compress( &cinfo );
+  jpeg_destroy_compress( &cinfo );
+  fclose( infile );
 }
 
 int main() {
-
-  // const int kSize = 256;
-  // char* name = "V90423-201212.mp4";
-
-  // char command[kSize];
-  // snprintf(command, kSize, "ffmpeg -i %s image.jpg", name);
-  // printf("%s", command);
-  // system(command);
-
-  //read_JPEG_file("image.jpg");
-  read_DCT_coef("1.jpg", "dct_coefs.txt");
-
+  const char* filename = "1.jpg";
+  open_DCT_coefs(filename, kModify);
+  open_DCT_coefs(filename, kRead);
   return 0;
 }
